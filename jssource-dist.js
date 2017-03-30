31795c260b92ec0e38b1ee06cfd30b39bf762e2e
SolarPolarMan <SolarPolarMan@gmail.com>
2017-03-30 19:08:20 +0100

    it works

--- jssource-dist.js
+++ jssource-dist.js
@@ -1,1 +1,1 @@
-function process(o){console.log(o)}
+function process(){$("#copy").css("display","block"),$("#result").css("display","block");var a="";for(Math.seedrandom($("#app").val()+$("#pass").val());a.length<8;)a+=chars[Math.floor(Math.random()*chars.length)];$("#result").text(a)}var chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";window.onload=function(){new Clipboard("#copy")};
--- jssource.js
+++ jssource.js
@@ -1,3 +1,19 @@
-function process(theForm){
-  console.log(theForm);
+var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+window.onload=function(){
+  new Clipboard('#copy');
+};
+
+function process(){
+  $("#copy").css('display', 'block');
+  $("#result").css('display', 'block');
+
+  var result = "";
+
+  Math.seedrandom($("#app").val()+$("#pass").val());
+
+  while (result.length < 8) {
+  result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  $("#result").text(result);
 };
