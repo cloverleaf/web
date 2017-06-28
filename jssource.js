685f655b1993ccdcefc763851b221f8d375a2094
SolarPolarMan <SolarPolarMan@gmail.com>
2017-06-28 22:21:56 +0100

    Version 0.35
    Added "pass" query string - to be documented (Mainly a hidden feature rn)

--- jssource.js
+++ jssource.js
@@ -7,8 +7,26 @@ var defaultMaxLength = 255; //ALl
 var minLength = defaultMinLength; //Are,
 var maxLength = defaultMaxLength; //Really
 
+//https://stackoverflow.com/a/901144
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
 //Setup
 window.onload=function(){
+
+  //Copy password from url if provided
+  $('#pass').focus();
+  $('#pass').val(getParameterByName('pass'));
+  $('#pass').blur();
+
   //Initialize the copy button
   new Clipboard('#copy');
 
