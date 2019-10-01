126758ebad25f25b390c523a41e785be8fba8460
Allie <13716824+ChildishGiant@users.noreply.github.com>
2019-10-01 16:03:36 +0100

    Added debug theme

--- jssource.js
+++ jssource.js
@@ -55,6 +55,40 @@ function checkDebug() {
 		link.setAttribute("type", "text/css");
 		link.setAttribute("href", "debug.css");
 		document.getElementsByTagName("head")[0].appendChild(link);
+
+		// Add select option for an all #ff00ff theme
+
+		themeData.testTheme = {
+			"accent": "#ff00ff",
+			"lightAccent": "#ff00ff",
+			"text": "#ff00ff",
+			"background": "#ff00ff",
+			"internal": "#ff00ff",
+			"incorrect": "#ff00ff",
+			"correct": "#ff00ff",
+			"inputColor": "#ff00ff",
+			"linkColor": "#ff00ff",
+			"highlightColor": "#ff00ff",
+			"underlineColor": "#ff00ff"
+		};
+
+		const themeOption = document.createElement("option");
+		themeOption.onclick = "changeTheme('testTheme')";
+		themeOption.id = "testTheme";
+		themeOption.innerHTML = "testTheme";
+		document.querySelector("#options .input-field select").appendChild(themeOption);
+
+		const css = "a[id=testTheme]{background-color: #ff00ff !important; border: 1px ;}a[id=testTheme] i {color: #ff00ff";
+		const style = document.createElement("style");
+		style.type = "text/css";
+		if (style.styleSheet) {
+			// This is required for IE8 and below.
+			style.styleSheet.cssText = css;
+		} else {
+			style.appendChild(document.createTextNode(css));
+		}
+		document.getElementsByTagName("head")[0].appendChild(style);
+
 	}
 
 	// If I'm testing, change the page title so I can tell the tabs apart
